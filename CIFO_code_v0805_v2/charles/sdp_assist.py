e1f19cd26b1a9a0019355e05a9c7c2b9c1b58c43
lffernandes91 <113448089+lffernandes91@users.noreply.github.com>
2023-05-08 17:38:03 +0100

    Add files via upload

---
+++ CIFO_code_v0805_v2/charles/sdp_assist.py
@@ -0,0 +1,57 @@
+from charles import Individual
+#from copy import deepcopy
+from data.sdp_data import nutrients, data
+from random import randint, uniform, choice #,random
+#from operator import attrgetter
+
+
+def get_fitness(self):
+    """A fitness function that returns the
+    price of the food if it meets the requirements, otherwise the fitness is 2000.
+    """
+    total_cost = 0
+    nutritional_values = [0] * 9  # Initialize nutritional values list with 0s
+
+    for index in self.representation:
+        total_cost += data[index][2]  # Accessing the price of the ingredient at the given index
+        for i in range(9):
+            nutritional_values[i] += data[index][i + 3]  # Accessing and accumulating nutritional values
+
+    # Check if the requirements are met
+    requirements_met = True
+    for i, (nutrient, min_req) in enumerate(nutrients):
+        if nutritional_values[i] < min_req:
+            requirements_met = False
+            break
+
+    return total_cost if requirements_met else 2000
+
+def get_neighbours(self):
+    neighbours = []
+    
+    for i in range(7):  # Change the number of neighbours generated
+        new_representation = self.representation.copy()
+        index_to_change = randint(0, len(self.representation) - 1)
+        new_value = choice(range(len(data)))
+        while new_value == self.representation[index_to_change]:
+            new_value = choice(range(len(data)))
+        new_representation[index_to_change] = new_value
+        
+        if uniform(0, 1) < 0.5:  # 50% chance to add a new ingredient
+            new_ingredient = choice(range(len(data)))
+            while new_ingredient in new_representation:
+                new_ingredient = choice(range(len(data)))
+            new_representation.append(new_ingredient)
+        else:  # 50% chance to remove an ingredient
+            if len(new_representation) > 1:  # Check if there is any ingredient to remove
+                index_to_remove = randint(0, len(new_representation) - 1)
+                new_representation.pop(index_to_remove)
+            
+        neighbour = Individual(representation=new_representation)
+        neighbours.append(neighbour)
+    
+    return neighbours
+
+# Monkey Patching
+Individual.get_fitness = get_fitness
+Individual.get_neighbours = get_neighbours
